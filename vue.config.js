83372138b19ac138052e87b64a802bb2306df0d3
LiXiang <993215231@qq.com>
2023-04-18 09:20:53 +0800

    上传头像业务写之前备份

--- vue.config.js
+++ vue.config.js
@@ -42,6 +42,7 @@ module.exports = {
     },
     // before: require('./mock/mock-server.js')
   },
+  
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
     // it can be accessed in index.html to inject the correct title.
